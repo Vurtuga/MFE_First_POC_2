7581d61b48a5d8700afdb22c3e69e55b83638e6b
Eng_Mohamed Khalifa <Eng_m.khalifa@outlook.com>
2023-05-01 15:09:26 +0300

    Create Auth Project

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import  { createRoot }  from 'react-dom/client';
+import App from './App'
+
+import {createMemoryHistory , createBrowserHistory} from 'history';
+
+
+const mount = (el,{onNavigate , defaultHistory}) =>{
+  
+    const history = defaultHistory || createMemoryHistory();
+
+    if(onNavigate){
+        history.listen(onNavigate);
+    }
+    const root = createRoot(el);
+    root.render(
+        <App history={history}/>
+    );
+
+    return {
+        onParentNavigate( {pathname : nextPathname}){
+            const {pathname} = history.location;
+
+            if(pathname !== nextPathname){
+                history.push(nextPathname);
+            }
+
+        }
+    }
+}
+
+if(process.env.NODE_ENV === 'development'){
+    const devRoot = document.querySelector("#_auth-dev-root")
+    if(devRoot){
+        mount(devRoot,{ defaultHistory: createBrowserHistory()});
+    }
+}
+
+export {mount};
